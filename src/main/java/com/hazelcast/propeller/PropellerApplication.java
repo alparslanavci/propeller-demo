4c140249db4054dfaa5ef47817b63fb796bb332c
Rafal Leszko <rafal@hazelcast.com>
2021-03-30 17:14:35 +0200

    Add sample Spring Boot project

---
+++ src/main/java/com/hazelcast/propeller/PropellerApplication.java
@@ -0,0 +1,13 @@
+package com.hazelcast.propeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropellerApplication.class, args);
+	}
+
+}
