ffcf64ccfd727a4c6eb4c58093a43030a3f0e26b
Rafal Leszko <rafal@hazelcast.com>
2021-03-30 18:40:41 +0200

    Add project files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# propeller-demo
+# Propeller - Project X March 2021
+
+You need to have a k8 cluster running.
+
+## Demo
+
+1. Start the services
+   
+    kubectl apply -f kubernetes.yaml
+    
+2. Observe all the services
+   
+   kubectl logs -f deployment.apps/plan-meeting
+   kubectl logs -f deployment.apps/book-flights
+   kubectl logs -f deployment.apps/book-hotel
+   kubectl logs -f deployment.apps/book-transport
+   
+3. Send a request to the first service
+
+    curl http://localhost:8080/action
+   
+If not run on Docker Desktop, then change `localhost` to your LoadBalancer IP.
+
+4. Observe services calling each other.
+
+![alt text](markdown/result.png "Result")
+    
---
+++ src/main/java/com/hazelcast/propeller/TransactionController.java
@@ -0,0 +1,104 @@
+package com.hazelcast.propeller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@RestController
+public class TransactionController {
+
+    @Value("${NEXT_SERVICE:#{null}}")
+    private String nextService;
+
+    @Value("${SHOULD_FAIL:#{null}}")
+    private Boolean shouldFail;
+
+    @Value("${SLEEP_TIME_MS:#{null}}")
+    private Integer sleepTimeMs;
+
+    @RequestMapping("/action")
+    public String action(@RequestHeader(value = "transactionId", required = false) String transactionId) throws InterruptedException {
+        System.out.println();
+        if (transactionId != null) {
+            System.out.println("Execution action with transactionId '" + transactionId + "'...");
+        }
+        System.out.println("Execution action...");
+        System.out.println();
+
+        // sleep
+        Integer sleepTimeTotalMs = sleepTimeMs;
+        if (sleepTimeTotalMs == null) {
+            sleepTimeTotalMs = 3000;
+        }
+        int sleepTimePart = sleepTimeTotalMs / 20;
+        for (int i = 0; i < 20; i++) {
+            System.out.println("|");
+            Thread.sleep(sleepTimePart);
+        }
+        System.out.println("v");
+        System.out.println("DONE");
+        System.out.println();
+
+        // call other service
+        if (nextService != null) {
+            System.out.println("Calling next service '" + nextService + "'...");
+            System.out.println();
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("transactionId", transactionId != null ? transactionId : generateTransactionId());
+            HttpEntity entity = new HttpEntity(headers);
+            new Thread(
+                    () -> {
+                        try {
+                            restTemplate.exchange("http://" + nextService + ":8080/action", HttpMethod.GET, entity, String.class);
+                        } catch (Exception e) {
+                            // ignore
+                        }
+                    }
+            ).start();
+        }
+
+        // success or fail
+        if (shouldFail != null && shouldFail != false) {
+            throw new RuntimeException("Service execution failed!");
+        } else {
+            return "Action executed";
+        }
+    }
+
+    private String generateTransactionId() {
+        return UUID.randomUUID().toString();
+    }
+
+    @RequestMapping("/compensation")
+    public String compensation() throws InterruptedException {
+        System.out.println();
+        System.out.println("Execution compensation action...");
+        System.out.println();
+
+        // sleep
+        Integer sleepTimeTotalMs = sleepTimeMs;
+        if (sleepTimeTotalMs == null) {
+            sleepTimeTotalMs = 3000;
+        }
+        int sleepTimePart = sleepTimeTotalMs / 20;
+        for (int i = 0; i < 20; i++) {
+            System.out.println("|");
+            Thread.sleep(sleepTimePart);
+        }
+        System.out.println("v");
+        System.out.println("DONE");
+        System.out.println();
+
+        return "Compensation executed";
+    }
+
+}
